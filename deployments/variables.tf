e0f35f9eb48888be5d479f78c37e1f27478bea15
GraysonCAdams <grayson.adams@turner.com>
2020-11-01 23:47:06 -0500

    Required variables updated

--- deployments/variables.tf
+++ deployments/variables.tf
@@ -9,3 +9,36 @@ variable "nfs_mount_path" {
 variable "nfs_storageclass" {
   type = string
 }
+
+variable "chartsmuseum_fqdn" {
+  type = string
+}
+
+variable "docker_registry_fqdn" {
+  type = string
+}
+
+variable "pihole_fqdn" {
+  type = string
+}
+
+variable "metallb_pool_name" {
+  type = string
+}
+
+variable "metallb_start_ip" {
+  type = string
+}
+
+variable "metallb_end_ip" {
+  type = string
+}
+
+variable "pihole_metallb_ip" {
+  type = string
+}
+
+variable "pihole_password" {
+  type        = string
+  description = "Admin password for accessing Pi-Hole"
+}
