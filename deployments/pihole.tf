f4015c4352d7e783aecb69bf9cc84e11c8c5d74d
GraysonCAdams <grayson.adams@turner.com>
2020-11-01 23:46:58 -0500

    Pihole added (MetalLB support)

---
+++ deployments/pihole.tf
@@ -0,0 +1,21 @@
+# https://github.com/MoJo2600/pihole-kubernetes/tree/master/charts/pihole
+
+resource "helm_release" "pihole" {
+  name       = "pihole"
+  repository = "https://mojo2600.github.io/pihole-kubernetes/"
+  chart      = "pihole"
+  version    = "1.7.19"
+  namespace  = "pihole"
+  depends_on = [kubernetes_namespace.pihole]
+  values = [
+    "${templatefile("${path.module}/pihole.yaml.tmpl",
+      {
+        nfs_storageclass : var.nfs_storageclass,
+        metallb_pool_name: var.metallb_pool_name,
+        pihole_metallb_ip: var.pihole_metallb_ip,
+        pihole_fqdn : var.pihole_fqdn,
+        pihole_password : var.pihole_password
+      }
+    )}"
+  ]
+}
